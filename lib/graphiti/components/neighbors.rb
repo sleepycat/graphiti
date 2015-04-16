89c1c111bd03c3ed806918cf85089252d07e9375
Mike Williamson <mike@korora.ca>
2015-04-16 15:13:24 -0400

    Correct typo in neighbors options
    Apparently Arango is now throwing a error if you mangle one of the
    options, for a traversal.

--- lib/graphiti/components/neighbors.rb
+++ lib/graphiti/components/neighbors.rb
@@ -9,7 +9,7 @@ module Graphiti
     def initialize list, options = {}
       @graph_name = Graphiti.config.graph
       @options_name = SecureRandom.hex(6)
-      options = options.merge(uniqueness: {edges: "global", vertices: "globals"})
+      options = options.merge(uniqueness: {edges: "global", vertices: "global"})
       @aql = "(FOR i IN (#{list.aql}) FOR n IN i RETURN GRAPH_NEIGHBORS(@graph_name, n, @#{@options_name})[*].vertex)"
 
       @bind_vars = list.bind_vars.merge('graph_name' => @graph_name, @options_name.to_s => options)
